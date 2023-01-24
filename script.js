1aaac7fbd9a4b896d566052365e831ebd628ee37
Michael Ciao <michaelciao@Michaels-MacBook-Pro-8.local>
2023-01-23 22:23:20 -0600

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunc(){
+    console.log("hi");
+    var members = document.getElementById("members");
+
+    var input = document.getElementById("inp");
+    //li.text = "hi";
+    members.innerHTML = "<li>"+input.value+"</li>";
+}
+window.onload = function(){
+    var button = document.getElementById("button");
+    button.onclick = myFunc;
+}
+console.log("hi1");
